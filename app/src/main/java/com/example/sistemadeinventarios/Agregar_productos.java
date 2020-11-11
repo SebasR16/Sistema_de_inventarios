e86219831b192153332a18687768b83a31f72ed0
Sebas <sebastianrnoriega.hotmail.com>
2020-11-11 02:40:56 -0700

    diseño de provedores y productos preeliminar

---
+++ app/src/main/java/com/example/sistemadeinventarios/Agregar_productos.java
@@ -0,0 +1,14 @@
+package com.example.sistemadeinventarios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Agregar_productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_productos);
+    }
+}
--- app/src/main/java/com/example/sistemadeinventarios/Menu_principal.java
+++ app/src/main/java/com/example/sistemadeinventarios/Menu_principal.java
@@ -3,12 +3,37 @@ package com.example.sistemadeinventarios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu_principal extends AppCompatActivity {
-
+    Button productos,proveedores,ventas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        productos = (Button) findViewById(R.id.btnproductos);
+        proveedores = (Button) findViewById(R.id.btnproveedores);
+
+        productos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(v.getContext(), Agregar_productos.class);
+                startActivityForResult(intent, 0);
+                finish();
+            }
+        });
+        proveedores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(v.getContext(), Agregar_proveedores.class);
+                startActivityForResult(intent, 0);
+                finish();
+            }
+        });
+
     }
 }
